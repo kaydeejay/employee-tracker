e6e611fd45b4c012ffa3ebba7de4b8263a45e2e7
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-02-21 12:46:22 -0500

    continues building view by dept function

--- app.js
+++ app.js
@@ -30,4 +30,20 @@ exports.readDB = (str) => {
     console.log(response);
     mainMenu.mainMenu();
   });
-} 
+}
+
+exports.listDepts = () => {
+  connection.query('SELECT * FROM departments', async (err,data) => {
+    if (err) throw err;
+    const response = await new Promise((res) => {
+      res(data);
+    });
+    // trying to return a filled departments array to the outer function
+    const departmentsArr = [];
+    for (x in response) {
+      departmentsArr.push(response[x].department);
+    }
+    // console.log(departments);
+  });
+  return departments;
+}
