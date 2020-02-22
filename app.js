aa6d2ce8ca815c37ea5d821bcc4c4d32d56b17f9
Kevin Devine-Jones <kevin.jones215@gmail.com>
2020-02-22 06:34:42 -0500

    impliments everything but employee update and delete

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const mysql = require('mysql');
 const mainMenu = require('./lib/main-menu');
+const followUps = require('./lib/follow-ups')
 
 const connection = mysql.createConnection({
   host: "localhost",
@@ -21,6 +22,8 @@ connection.connect(async (err) => {
   mainMenu.mainMenu();
 });
 
+// ======== READ functions ========
+
 exports.readDB = (str) => {
   connection.query(str, async (err,data) => {
     if (err) throw err;
@@ -32,26 +35,62 @@ exports.readDB = (str) => {
   });
 }
 
+exports.viewAllRows = (str) => {
+  return new Promise ((res) => {
+    let query = `SELECT * FROM ${str};`;
+    connection.query(query, (err,data) => {
+      if (err) throw err;
+      res(data);
+    });
+  });
+}
+
+exports.listXByY = (str1,str2) => {
+  connection.query(
+    `SELECT * FROM employees WHERE ${str1} = "${str2}";`,
+    (err,data) => {
+      if (err) throw err;
+      console.table(data);
+      mainMenu.mainMenu();
+  });
+}
+
+// ======== CREATE functions ========
+
+exports.createNewEmployee = (obj) => {
+  console.log(obj);
+  connection.query('INSERT INTO employees SET ?', 
+  {
+      firstName: obj.firstName,
+      lastName: obj.lastName,
+      title: obj.role,
+      department: obj.dept,
+      salary: obj.salary,
+      manager: obj.manager,
+  }, (err) => {
+    if (err) throw err;
+    console.log('Employee Successfully Added');
+    mainMenu.mainMenu();
+  });
+}
+
+exports.addNew = (str1, str2) => {
+  connection.query(`INSERT INTO ${str1} SET ${str1} = "${str2}"`,
+  (err) => {
+    if (err) throw err;
+    console.log('Successfully Added');
+    mainMenu.mainMenu();
+  });
+}
+
+// ======== UPDATE functions ========
+
+
+
+// ======== END function ========
 
 exports.endConnection = () => {
   connection.end();
 }
 
-// ======== function graveyard ========
-
-// exports.viewAllDepts = () => {
-//   return new Promise ((res) => {
-//     let query = 'SELECT * FROM departments;';
-//     connection.query(query, (err,data) => {
-//       if (err) throw err;
-//       res(data);
-//     });
-//   }).then((result) => {
-//     let departments = [];
-//     for (x in result) {
-//       departments.push(result[x].department);
-//     }
-//     console.table(departments);
-//     mainMenu.mainMenu();
-//   });
-// }
+  // ======== function graveyard ========
